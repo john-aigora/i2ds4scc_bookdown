b46082213b40e6cd549d1bd0e59f0895088a7695
BoaSHoaK <thierryworch@hotmail.com>
2021-10-18 22:57:41 +0200

    Update the section on Excel

--- temp/script auto reporting.R
+++ temp/script auto reporting.R
@@ -39,7 +39,6 @@ wb <- openxlsx::createWorkbook()
   # Add a new worksheet
 addWorksheet(wb, sheetName = "Mean", gridLines = FALSE)
 
-
   # Creating the Sensory Profiles with some Product Information
 p_info <- read_xlsx(file_path, sheet = "Product Info") %>% 
   dplyr::select(-Type)
@@ -54,7 +53,7 @@ senso_mean <- sensory %>%
   pivot_wider(names_from=Attribute, values_from=Score, values_fn=mean)
 
 
-  # Exporting the Results to Excel
+  # Exporting the Results to Excel (automated formatting)
 writeDataTable(wb,
                sheet = "Mean",
                x = senso_mean, 
@@ -62,4 +61,79 @@ writeDataTable(wb,
                startRow = 1,
                colNames = TRUE, rowNames = TRUE, 
                tableStyle = "TableStyleLight9")
+
+openXL(wb)
+
+  # Exporting the Results to Excel (manual formatting)
+
+    # Pre-define options to control the borders 
+options("openxlsx.borderColour" = "#4F80BD")
+options("openxlsx.borderStyle" = "thin")
+
+    # Automatically set Number formats to 3 values after the decimal
+options("openxlsx.numFmt" = "0.0")
+
+    # Change the font to Calibri size 10
+modifyBaseFont(wb,fontName = "Calibri", fontSize = 10)
+
+    # Header Style (blue background, top/bottom borders, text centered/bold)
+headSty <- createStyle(fgFill = "#DCE6F1",
+                       border = "TopBottom",
+                       halign = "center",
+                       textDecoration = "bold")
+
+    # Add new worksheet
+addWorksheet(wb, sheetName = "Mean (manual formatting)", gridLines = FALSE)
+
+    # Freeze Panel
+freezePane(wb, sheet=2, firstRow=TRUE, firstCol=TRUE)
+
+writeData(wb,
+          sheet =2,
+          x = senso_mean, 
+          startCol = 1,
+          startRow = 1,
+          colNames = TRUE, rowNames = FALSE, 
+          headerStyle = headSty)
+
+
+
+openXL(wb)
+
+  # Conditional Formatting
+
+    # Styles for conditional formatting
+pos_style <- createStyle(fontColour = "firebrick3", bgFill = "mistyrose1")
+neg_style <- createStyle(fontColour = "navy", bgFill = "lightsteelblue")
+
+    # Compute the overall mean
+overall_mean <- senso_mean %>% 
+  summarize(across(where(is.numeric), mean))
+
+    # Create the worksheet and write the Data
+addWorksheet(wb, sheetName = "Conditional Formatting", gridLines=FALSE)
+writeDataTable(wb, sheet = 3, x = senso_mean, 
+               startCol = 1, startRow = 1,
+               colNames = TRUE, rowNames = FALSE)
+
+for (v in 1:ncol(overall_mean)){
+  
+  conditionalFormatting(wb,
+                        sheet = 3,
+                        cols  = v + 3,
+                        rows  = 1 + 1:nrow(senso_mean), 
+                        rule  = paste0(">", overall_mean[1,v]),
+                        style = pos_style)
+  
+  conditionalFormatting(wb,
+                        sheet = 3,
+                        cols  = v + 3,
+                        rows  = 1 + 1:nrow(senso_mean), 
+                        rule  = paste0("<", overall_mean[1,v]),
+                        style = neg_style)
+  
+}
+
+setColWidths(wb, sheet = 3,
+             cols = 1:ncol(senso_mean), widths = 12)
 
