e641aadadeb23237da41a998d031d48488c9766d
BoaSHoaK <thierryworch@hotmail.com>
2022-02-14 17:36:05 +0100

    Finalize the review of data manipulation

--- temp/script thierry data manip.R
+++ temp/script thierry data manip.R
@@ -106,3 +106,70 @@ melt(sensory)
 sensory %>% 
   dplyr::select(Judge, Product, Shiny) %>% 
   pivot_wider(names_from = Judge, values_from = Shiny)
+
+# Transformation that Alters the Data -------------------------------------
+
+sensory %>% 
+  summarise(across(where(is.numeric), mean))
+
+sensory %>% 
+  summarise(across(where(is.numeric), list(min=min, max=max)))
+
+sensory %>% 
+  group_by(Product) %>% 
+  summarise(across(where(is.numeric), mean)) %>% 
+  ungroup()
+
+sensory %>% 
+  group_by(Product) %>% 
+  summarise(across(Shiny:Melting, mean)) %>% 
+  ungroup()
+
+sensory_attr <- colnames(sensory)[5:ncol(sensory)]
+sensory %>% 
+  group_by(Product) %>% 
+  summarise(across(all_of(sensory_attr), mean)) %>% 
+  ungroup()
+
+sensory %>% 
+  pivot_longer(Shiny:Melting, names_to="Attribute", values_to="Scores") %>% 
+  mutate(Attribute = fct_inorder(Attribute)) %>% 
+  group_by(Product, Attribute) %>% 
+  summarise(Scores = mean(Scores)) %>% 
+  pivot_wider(names_from=Attribute, values_from=Scores) %>% 
+  ungroup()
+
+sensory %>% 
+  pivot_longer(Shiny:Melting, names_to="Attribute", values_to="Scores") %>% 
+  dplyr::select(-Judge) %>% 
+  pivot_wider(names_from=Attribute, values_from=Scores)
+
+sensory %>% 
+  pivot_longer(Shiny:Melting, names_to="Attribute", values_to="Scores") %>% 
+  dplyr::select(-Judge) %>% 
+  pivot_wider(names_from=Attribute, values_from=Scores, values_fn=mean)
+
+# Combining Data ----------------------------------------------------------
+
+  ## By rows
+path <- file.path("data", "excel_scrap.xlsx")
+
+session1 <- read_xlsx(path, sheet=1)
+session2 <- read_xlsx(path, sheet=2) 
+session3 <- read_xlsx(path, sheet=3)
+
+all_data <- bind_rows(session1, session2, session3, .id = "Session")
+
+  ## Bu Columns
+file_path <- here("data","Consumer Test.xlsx")
+excel_sheets(file_path)
+
+time <- read_xlsx(file_path, sheet="Time Consumption")
+weight <- read_xlsx(file_path, sheet="Weight")
+(consumption <- time %>% 
+    full_join(weight, by="Product") %>% 
+    mutate(Amount = `Nb biscuits`*Weight))
+
+(biscuits <- read_xlsx(file_path, sheet="Biscuits") %>% 
+  mutate(Consumer = str_c("J",Consumer)) %>% 
+  full_join(consumption, by=c("Consumer"="Judge", "Samples"="Product")))
