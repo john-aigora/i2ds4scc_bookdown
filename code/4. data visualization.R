16a2b6029d6d3828988372e3e34b6e2ff97108e6
BoaSHoaK <thierryworch@hotmail.com>
2022-09-29 22:06:11 +0200

    Finish the section on tables

--- code/4. data visualization.R
+++ code/4. data visualization.R
@@ -42,3 +42,56 @@ color_code <- ifelse(mean_sensory$Thickness <= 20, "blue",
                      ifelse(mean_sensory$Thickness >= 40, "red", "black"))
 flex_table_design %>% 
   color(j=~Thickness, color=color_code)
+
+# gt  ---------------------------------------------------------------------
+
+library(gt)
+
+file_path <- here("data","biscuits_consumer_test.xlsx") 
+
+mean_consumer <- read_xlsx(file_path, sheet="Time Consumption") %>%
+  dplyr::select(Product, `Time (min)`, `Nb biscuits`) %>%
+  separate(`Time (min)`, c("Min", "Sec"), sep="min") %>%
+  mutate(across(c("Min","Sec"), as.numeric)) %>% 
+  mutate(Time = Min+Sec/60) %>% 
+  group_by(Product) %>% 
+  summarise(across(c("Time", "Nb biscuits"), mean, na.rm = TRUE)) %>% 
+  ungroup()
+
+consumer_gt_table <- mean_consumer %>%
+  gt () %>%
+  cols_align(align = "center", columns = everything()) %>%
+  fmt_number(columns = c( "Time", "Nb biscuits") , decimals = 2) %>%
+  tab_header(title = md ("**Consumption time and number of biscuits eaten**"), 
+             subtitle = md ("*Average taken from 99 consumers*"))
+
+mean_consumer_2 <- mean_consumer %>%
+  dplyr::select(-Time) %>%
+  arrange(desc(`Nb biscuits`))
+
+note <- str_c("Avg. consumption time: ", round(mean(mean_consumer$Time),2), " min")
+
+consumption <- mean_consumer_2 %>%
+  gt () %>%
+  cols_align(align = "center", columns = everything()) %>%
+  fmt_number(columns = "Nb biscuits" , decimals = 2) %>%
+  tab_header(title = md ("**Number of biscuits eaten**"), 
+             subtitle = md ("*Average taken from 99 consumers*")) %>%
+  tab_source_note(source_note = note)
+
+nb_range <- range(mean_consumer_2$`Nb biscuits`)
+
+library(scales)
+consumption %>% 
+  data_color(columns=`Nb biscuits`, colors=col_numeric(c("#FEF0D9","#990000"), domain=nb_range, alpha=0.75))
+
+library(gtExtras)
+mean_consumer_2 %>%
+  mutate(`Nb biscuits (%)` = 10*`Nb biscuits`) %>% 
+  gt () %>%
+  cols_align(align = "center", columns = everything()) %>%
+  fmt_number(columns = "Nb biscuits" , decimals=2) %>%
+  tab_header(title = md ("**Number of biscuits eaten**"), 
+             subtitle = md ("*Average taken from 99 consumers*")) %>%
+  tab_source_note(source_note = note) %>% 
+  gt_plt_bar_pct(`Nb biscuits (%)`, scaled=TRUE)
