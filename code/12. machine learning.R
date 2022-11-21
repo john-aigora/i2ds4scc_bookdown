769fbb59bc0349b108dbab3c579e565e1708d88e
Vanessa Souza <vanessa.souza@aigora.com>
2022-11-21 17:25:08 -0500

    ML code updated

--- code/12. machine learning.R
+++ code/12. machine learning.R
@@ -10,22 +10,17 @@ wine <- rattle::wine %>%
 # Unsupervised ML ---------------------------------------------------------
 
   ## Dimensionality Reduction
-library (FactoMineR)
+library(FactoMineR)
+library (factoextra)
+
 res_pca <- PCA(wine, quali.sup=1, scale.unit=TRUE, graph=FALSE)
 
-library(factoextra)
-fviz_eig(res_pca, addlabels=TRUE, ylim=c(0,50))
-fviz_pca_biplot(pca_results, repel=TRUE, label="var", col.var="red", col.ind="black")
+fviz_pca_biplot(res_pca, repel=TRUE, label="var", col.var="red", col.ind="black")
 
-pca_plot <- fviz_pca_biplot(pca_results, repel = TRUE,
-                            label = "var",
-                            col.var = "red",
-                            col.ind = "black")
-
-wine_reduced <- as_tibble(pca_results$ind$coord[,1:2])
+wine_reduced <- as_tibble(res_pca$ind$coord[,1:2])
 
   ## Clustering
-fviz_nbclust(wine_reduced, kmeans, method="wss")
+fviz_nbclust(wine_reduced, kmeans, method = "wss")
 
 set.seed(123)
 wine_kmeans <- kmeans(wine_reduced, centers=3, nstart=20)
@@ -46,11 +41,11 @@ wine_testing <- testing(wine_split)
 wine_cv <- wine_train %>% 
   vfold_cv(v=5, strata=Type)
 
-  ## Create the Model
+  ## Data Preprocessing
 model_recipe <- wine_train %>% 
   recipe(Type ~ .)
 
-  ## ML algorithm specification
+  ## Model definition
 rf_spec <- rand_forest(
   mtry = tune(),
   trees = tune(),
@@ -63,11 +58,12 @@ rf_wf <- workflow() %>%
   add_recipe(model_recipe) %>% 
   add_model(rf_spec)
 
-  ## Parameters tuning
+  ## Tuning the parameters
 params_grid <- rf_spec %>%
   parameters() %>%
-  update(mtry=mtry(range=c(1,2)), trees=trees(range=c(10,200))) %>% 
-  grid_regular(levels = 5)
+  update(mtry = mtry(range = c(1,2)),
+         trees = trees(range = c(10,200))) %>% 
+  grid_regular(levels=5)
 
 tuning <- tune_grid(rf_wf, resamples=wine_cv, grid=params_grid)
 
@@ -79,9 +75,29 @@ final_model <- rf_wf %>%
   finalize_workflow(params_best) %>%
   fit(wine_train)
 
-  ## Assess Model Quality
+  ## Model evaluation
 obs_vs_pred <- wine_testing %>%
   bind_cols(predict(final_model, .))
 
 cm <- conf_mat(obs_vs_pred, Type, .pred_class)
 autoplot(cm, type = "heatmap")
+
+obs_vs_pred_prob <- bind_cols(wine_testing %>% select(Type), predict(final_model, wine_testing, type = "prob")) %>% 
+  mutate(Type = as.factor(Type))
+
+accuracy(obs_vs_pred, truth = "Type", estimate = ".pred_class")
+kap(obs_vs_pred, truth = "Type", estimate = ".pred_class")
+
+library(modelStudio)
+
+data_to_explain <- wine_train
+
+explainer <- explain_tidymodels(
+  model = final_model, 
+  data = data_to_explain %>% 
+    select(-Type),
+  y = data_to_explain$Type
+)
+
+var_imp <- variable_importance(explainer, loss_function = loss_cross_entropy, type = "ratio")
+plot(var_imp)
